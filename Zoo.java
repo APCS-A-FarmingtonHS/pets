d87602a8ab4eaf2f8777ea6fb4b4ee584937c50f
Tim Barron <barront@fpsct.org>
2017-02-17 09:17:25 -0500

    Update Zoo.java

--- Zoo.java
+++ Zoo.java
@@ -14,7 +14,26 @@ public class Zoo
     {
         theZoo = new Pet[40];
         theZoo[0] = new Pet("boring pet");
-        theZoo[3] = new DireWolf();
+        theZoo[1] = new Pet("boring pet");
+        theZoo[2] = new Pet("boring pet");
+        theZoo[3] = new Pet("boring pet");
+        theZoo[4] = new Pet("boring pet");
+        theZoo[5] = new Pet("boring pet");
+        theZoo[6] = new Pet("boring pet");
+        theZoo[7] = new Pet("boring pet");
+        theZoo[8] = new Pet("boring pet");
+        theZoo[9] = new Pet("boring pet");
+        theZoo[10] = new Pet("boring pet");
+        theZoo[11] = new Pet("boring pet");
+        theZoo[12] = new Pet("boring pet");
+        theZoo[13] = new Pet("boring pet");
+        theZoo[14] = new Pet("boring pet");
+        theZoo[15] = new Pet("boring pet");
+        theZoo[16] = new Pet("boring pet");
+        theZoo[17] = new Pet("boring pet");
+        theZoo[18] = new Pet("boring pet");
+        theZoo[19] = new DireWolf();
+        theZoo[20] = new Pet("boring pet");
     }
 
     public void soundOff()
