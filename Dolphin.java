a1d740b5f5fc34b0eb9d26e739a160aa8523cd5b
kd319 <19darshku@fpsct.org>
2017-02-17 09:44:08 -0500

    Added the Dolphin Class

---
+++ Dolphin.java
@@ -0,0 +1,22 @@
+public class Dolphin extends Pet
+{
+    public Dolphin ()
+    {
+        super("jimmy");  
+    }
+    
+    public Dolphin (String name)
+    {
+        super(name);
+}
+
+    public String speak()
+    {
+        return "click click click";
+}
+  
+    public String color()
+    {
+        return "grey";
+}
+}
